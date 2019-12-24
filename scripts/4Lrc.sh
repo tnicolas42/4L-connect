29452caafe40cc9e2c8eadec9bd40bb58725127f
tnicolas42 <tnicolas@student.42.fr>
2019-12-24 17:02:51 +0000

    update initialisation

--- scripts/4Lrc.sh
+++ scripts/4Lrc.sh
@@ -1,5 +1,2 @@
-DIR="`git rev-parse --show-toplevel`"
-
-export DIR4L=$DIR  # path of 4L files
 export TMUX_SESSION=4L  # name of th tmux session
 export LAUNCH_AT_START=0  # launch the server on startup
--- scripts/init.sh
+++ scripts/init.sh
@@ -6,3 +6,14 @@ SOURCE="source $DIR/scripts/4Lrc.sh"
 if [[ "`cat ~/.zshrc | grep $SOURCE`" == "" ]]; then
 	echo "$SOURCE" >> ~/.zshrc
 fi
+
+DIR4L="export DIR4L=$DIR"
+if [[ "`cat ~/.zshrc | grep $DIR4L`" == "" ]]; then
+	echo "$DIR4L" >> ~/.zshrc
+fi
+
+pip3 install -r "$DIR/requirements.txt"
+
+python3 $DIR/djangoWebsite/manage.py makemigrations
+python3 $DIR/djangoWebsite/manage.py migrate
+python3 $DIR/djangoWebsite/manage.py createsuperuser

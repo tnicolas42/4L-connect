72fbe7d90c377e4b910c6cad3d275e6a9be8abb5
tnicolas42 <tnicolas@student.42.fr>
2020-02-02 13:39:44 +0100

    update kill script

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
+if [[ -z "${TMUX_SESSION}" ]]; then
+  TMUX_SESSION="4L"  # set a default value if the env variable doesn't exist
+else
+  TMUX_SESSION="${TMUX_SESSION}"
+fi
+
 tmux kill-session -t $TMUX_SESSION
